b82aca386da9b31b745ef76b727d6b71c2dbf8de
Alozie Miracle Patrick <nacotech09@gmail.com>
2025-10-21 10:37:17 +0100

    updated appwrite

--- constant/endpoint.ts
+++ constant/endpoint.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:5000/api/v1";
+// export const BASE_URL = "http://localhost:5000/api/v1";
 
 export const BASE_URL = "https://lume-ai-backend.vercel.app/api/v1";
